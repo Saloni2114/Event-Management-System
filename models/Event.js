e7c570b53e8ceb57969c5c57802515a212cb22b6
Saloni2114 <salonikathpal2114@gmail.com>
2026-04-22 00:55:55 +0530

    Created event CRUD APIs

--- models/Event.js
+++ models/Event.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  date: String,
-  location: String
-});
+  title: {
+    type: String,
+    required: [true, "Please provide a title"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide a description"],
+  },
+  date: {
+    type: Date,
+    required: [true, "Please provide a date"],
+  },
+  location: {
+    type: String,
+    required: [true, "Please provide a location"],
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Event", eventSchema);
