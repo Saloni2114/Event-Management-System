f0dddb332b2bae76b1d86c4b9b98131f8238bb51
Saloni2114 <salonikathpal2114@gmail.com>
2026-04-21 16:19:06 +0530

    Connected MongoDB database successfully

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("API Running 🚀");
