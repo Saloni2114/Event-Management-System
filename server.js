aff41ff4552b129e42277113e253cb3109394617
Saloni2114 <salonikathpal2114@gmail.com>
2026-04-21 16:47:45 +0530

    Connected MongoDB database successfully

--- server.js
+++ server.js
@@ -3,12 +3,26 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+console.log("SERVER FILE RUNNING ✅");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url);
+  next();
+});
+
+// ✅ ADD HERE (VERY IMPORTANT POSITION)
+app.get("/hello", (req, res) => {
+  res.send("Hello working");
+});
+
+// ✅ AUTH ROUTE
+app.use("/api/auth", require("./routes/authRoutes"));
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
   .catch(err => console.log(err));
@@ -17,7 +31,7 @@ app.get("/", (req, res) => {
   res.send("API Running 🚀");
 });
 
-const PORT = 5000;
+const PORT = 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
